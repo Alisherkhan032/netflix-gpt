7bbe726cb112d22fc310a9041f5116b00c9fb546
Ali Sher Khan <112810526+Alisherkhan032@users.noreply.github.com>
2024-10-21 15:45:51 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,6 +6,6 @@ export const API_OPTIONS ={
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmNhYmNlZjU2M2RmNzI4MDZhYmQ1NGQ5ODFlMjBlMSIsIm5iZiI6MTcyOTQ0Mzg5MS4zMzQ0OTUsInN1YiI6IjY3MTQxODk0NjUwMjQ4YjlkYjYyNDAyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZDdBUhMahReE-oQQgwo3hpHrT8Zr4p3mt54mYHUZOS4'
+        Authorization: ''
       }
   };
