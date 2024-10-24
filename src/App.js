c5a9ad1aaa27a09f678c793556e4a7972d87aaa8
alisherkhann <sherkhanali032@gmail.com>
2024-10-25 00:30:01 +0530

    removed logs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 
 function App() {
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
   return (
     <>
       <Provider store={appStore}>
--- src/component/GptMovieSuggestion.jsx
+++ src/component/GptMovieSuggestion.jsx
@@ -5,16 +5,12 @@ import MovieList from './MovieList';
 const GptMovieSuggestion = () => {
   const {movieNames, movieResults} = useSelector((store) => store.gpt)
 
-  console.log('moviesNames r', movieNames);
-
   if(!movieNames) return null;
   return (
     <div className='p-4 m-4 bg-black bg-opacity-80 text-white'>
-     <div>
-        {movieNames.map((movieName, idx)=>(
+      {movieNames.map((movieName, idx)=>(
           <MovieList key={idx} title={movieName}  movies={movieResults[idx]} />
-        ))}
-     </div>
+      ))}
     </div>
   )
 }
